7b4daf584a3ada7cf43247b6c4e93f405f4b8fea
Hemanth <hemanthr2053@gmail.com>
2022-07-29 14:09:14 +0530

    angular form validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  registerForm!:FormGroup
   title = 'form-valid';
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder){ }
+
+  ngOnInit(){
+    //validation
+
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required,Validators.minLength(4)]],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required,Validators.minLength(8)]]
+    })
+  }
+
+  onSubmit(){
+    this.submitted = true
+
+    if(this.registerForm.invalid){
+      return
+    }
+    alert("Submitted")
+  }
 }
